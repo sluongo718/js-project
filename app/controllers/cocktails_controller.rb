423ade881e683dea99a8f8f7884458b9cbc352ec
sluongo718 <sluongo718@yahoo.com>
2021-07-21 00:36:59 -0400

    set up cocktail controller:

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,26 @@
 class CocktailsController < ApplicationController
+
+    def index 
+        cocktails = Cocktail.all 
+
+        render json: CocktailSerializer.new(cocktails)
+    end
+
+    def create 
+        cocktail = Cocktail.new(cocktail_params)
+
+        if cocktail.save 
+
+            render json: CocktailSerializer.new(cocktail), status: :accepted
+        else
+            render json: {errors: cocktail.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    private 
+
+    def cocktail_params
+        params.require(:cocktail).permit(:name, :image, :instructions)
+    end
+
 end
