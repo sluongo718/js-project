00bb10376ec59d0a48d1bf16f83722761fef8dc8
sluongo718 <sluongo718@yahoo.com>
2021-07-21 00:53:48 -0400

    ingredients controller setup

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,25 @@
 class IngredientsController < ApplicationController
+    def index
+        ingreds = Ingredient.all 
+
+        render json: ingreds
+    end
+
+    def create 
+        ingred = Ingredient.new(ingredient_params)
+
+        render json: ingred
+    end
+
+    def destroy 
+        ingredient = Ingredient.find(params[:id])
+        ingredient.destroy
+    end
+
+    private 
+    
+    def ingredient_params
+        params.require(:ingredient).permit(:name, :cocktail_id)
+    end
+
 end
